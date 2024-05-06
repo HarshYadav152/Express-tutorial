9869800f704e61a893f7d61ee32db563fa1b4400
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2024-05-06 10:50:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("This is index.js");
+
+// const mod = require("./mod");
+// console.log(mod([2,3])); // print 2.5
+
+// console.log(mod);
+
+const mod = require("./mod");
+console.log(mod.avg([2,3])); // print 2.5
+
+console.log(mod.name) 
